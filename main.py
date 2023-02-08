b91d20ad419468507975458b36851ca03324cc9b
Biplo12 <robertbiplosek@gmail.com>
2023-02-08 21:52:49 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+discord_app_id="1072978460781330523"
+discord_public_key="6325d906c8301a1a6c48333ac3f45d75b5e044fba3827fdc3f6c84e711d4c748"
+discord_bot_token="MTA3Mjk3ODQ2MDc4MTMzMDUyMw.Gq-LS8.ixNFnu0sjmR07FKObfqjAj4aK-_womAEYHddto"
+discord_scopes_url="https://discord.com/api/oauth2/authorize?client_id=1072978460781330523&permissions=2147485696&scope=bot%20applications.commands"
+
+def clearConsole():
+    os.system('cls')
+
+def main():
+    clearConsole()
+    print("start")
+
+main()
